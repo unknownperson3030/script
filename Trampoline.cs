d2b77ef0fc84318825a35a095b701956ce726af5
unknownperson3030 <glebsurg@gmail.com>
2024-12-14 12:33:31 +0300

    Add files via upload

---
+++ Trampoline.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent<Jump>().jumpStrength = 10;
+    }   
+
+    
+    void OnTriggerExit(Collider other)
+    {
+        other.GetComponent<Jump>().jumpStrength = 2;
+    }
+}
